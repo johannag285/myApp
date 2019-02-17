e0fae31264825a1955e5302a6ff0331b776aee5a
johannag285 <johannag285@gmail.com>
2019-02-16 21:56:54 -0500

    add package-lock.json and add page perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },  { path: 'perfil', loadChildren: './perfil/perfil.module#PerfilPageModule' },
+
 ];
 
 @NgModule({
