f746e82ecb6da2e86a09e450ab877e6e4fba7c23
johannag285 <johannag285@gmail.com>
2019-03-22 14:48:50 -0500

    add the import for Camera and the contructor build the private Declares a private variable of the camera type and build two methods for that take photo and get photo since galery

--- src/app/perfil-estudiante/perfil-estudiante.page.ts
+++ src/app/perfil-estudiante/perfil-estudiante.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-perfil-estudiante',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-estudiante.page.scss'],
 })
 export class PerfilEstudiantePage implements OnInit {
-
-  constructor() { }
+  myPhoto: any;
+  constructor(private camera: Camera) { }
 
   ngOnInit() {
+  }
+  takePhoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.myPhoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+  getPhoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: false,
+      allowEdit: true,
+      targetHeight:300,
+      targetWidth:300
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     this.myPhoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
   }
 
 }
