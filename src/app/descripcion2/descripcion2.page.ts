4e111ebd012f2f7cf3a91b788182d086b1680d08
johannag285 <johannag285@gmail.com>
2019-03-30 20:26:00 -0500

    add the page description for employ

---
+++ src/app/descripcion2/descripcion2.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-descripcion2',
+  templateUrl: './descripcion2.page.html',
+  styleUrls: ['./descripcion2.page.scss'],
+})
+export class Descripcion2Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
