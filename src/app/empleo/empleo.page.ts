6537d959d1ccb201dce4409a7083006c8c74a2d0
johannag285 <johannag285@gmail.com>
2019-04-06 20:55:26 -0500

    add the imports for Empleo and TodoService for in the method ngOnInit get all employsand assign the response of this method to the array of jobs

--- src/app/empleo/empleo.page.ts
+++ src/app/empleo/empleo.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Empleo, TodoService} from './../services/todo.service';
 @Component({
   selector: 'app-empleo',
   templateUrl: './empleo.page.html',
   styleUrls: ['./empleo.page.scss'],
 })
 export class EmpleoPage implements OnInit {
-
-  constructor() { }
+empleos: Empleo[];
+  constructor(private todoService:TodoService) { }
 
   ngOnInit() {
+    this.todoService.getEmpleos().subscribe(res =>{
+      this.empleos = res;
+      console.log(this.empleos);
+      });
   }
 
 }
