5ee215e48d879177ff8de4e7b61a194036ba9e3a
johannag285 <johannag285@gmail.com>
2019-04-13 20:34:04 -0500

    add the imports from angularFirestoreModule and AngularFireStorageModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
-import{ AngularFireModule} from 'angularfire2';
-import {environment} from '../environments/environment';
-import{ AngularFirestoreModule} from 'angularfire2/firestore';
-
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(),IonicStorageModule.forRoot()
-,    AppRoutingModule,AngularFireModule.initializeApp(environment.firebase),AngularFirestoreModule],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot()
+    , AppRoutingModule, AngularFireModule.initializeApp(environment.firebase), AngularFirestoreModule,AngularFireStorageModule],
   providers: [
     StatusBar,
     SplashScreen,
@@ -26,4 +26,4 @@ import{ AngularFirestoreModule} from 'angularfire2/firestore';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
