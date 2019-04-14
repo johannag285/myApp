fab01ebbfb86b3f230eb690f943051bf566ccaa9
johannag285 <johannag285@gmail.com>
2019-04-13 20:35:59 -0500

    add the mthods for get the files, upload files and delete files

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
-import{AngularFirestore,AngularFirestoreCollection} from 'angularfire2/firestore';
-import{Observable} from 'rxjs';
-import{map} from 'rxjs/operators';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
 
-export interface Todo{
-  id ?: string;
+export interface Todo {
+  id?: string;
   task: string;
-  priority : string;
+  priority: string;
 }
-export interface Empleo{
-  id ?: string;
+export interface Empleo {
+  id?: string;
   imagen: string;
-  cargo : string;
-  descripcion : string;
-  salario : string;
-  horario : string;
+  cargo: string;
+  descripcion: string;
+  salario: string;
+  horario: string;
 }
 
 
@@ -22,78 +24,116 @@ export interface Empleo{
   providedIn: 'root'
 })
 export class TodoService {
- private todosCollection: AngularFirestoreCollection<Todo>;
- private todos: Observable<Todo[]>;
+  private todosCollection: AngularFirestoreCollection<Todo>;
+  private todos: Observable<Todo[]>;
 
- private empleosCollection: AngularFirestoreCollection<Empleo>;
- private empleos: Observable<Empleo[]>;
+  private empleosCollection: AngularFirestoreCollection<Empleo>;
+  private empleos: Observable<Empleo[]>;
 
-  constructor(db: AngularFirestore) {
+  constructor(db: AngularFirestore, private dbFire: AngularFireDatabase, private afStorage: AngularFireStorage) {
     this.todosCollection = db.collection<Todo>('todos');
     this.empleosCollection = db.collection<Empleo>('empleos');
 
     this.todos = this.todosCollection.snapshotChanges().pipe(
-      map(actions =>{
-        return actions.map(a=>{
+      map(actions => {
+        return actions.map(a => {
           const data = a.payload.doc.data();
           const id = a.payload.doc.id;
-          return {id, ...data};
+          return { id, ...data };
         });
       })
     );
 
 
     this.empleos = this.empleosCollection.snapshotChanges().pipe(
-      map(actions =>{
-        return actions.map(a=>{
+      map(actions => {
+        return actions.map(a => {
           const data = a.payload.doc.data();
           const id = a.payload.doc.id;
-          return {id, ...data};
+          return { id, ...data };
         });
       })
     );
-   }
+  }
 
-  
+
 
   getTodos() {
     return this.todos;
   }
- 
+
   getTodo(id) {
     return this.todosCollection.doc<Todo>(id).valueChanges();
   }
- 
+
   updateTodo(todo: Todo, id: string) {
     return this.todosCollection.doc(id).update(todo);
   }
- 
+
   addTodo(todo: Todo) {
     return this.todosCollection.add(todo);
   }
- 
+
   removeTodo(id) {
     return this.todosCollection.doc(id).delete();
   }
 
-//métodos para el crud de empleo
+  //métodos para el crud de empleo
   getEmpleos() {
     return this.empleos;
   }
- 
+
   getEmpleo(id) {
     return this.empleosCollection.doc<Empleo>(id).valueChanges();
   }
- 
+
   updateEmpleo(empleo: Empleo, id: string) {
     return this.empleosCollection.doc(id).update(empleo);
   }
- 
+
   addEmpleo(empleo: Empleo) {
     return this.empleosCollection.add(empleo);
   }
- 
+
   removeEmpleo(id) {
     return this.empleosCollection.doc(id).delete();
+  }
+
+  //métodos para actualizar, cargar y elminar una imagen
+  getFiles() {
+    let ref = this.dbFire.list('files');
+
+    return ref.snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      })
+    );
+  }
+
+  uploadToStorage(information): AngularFireUploadTask {
+    let newName = `${new Date().getTime()}.txt`;
+
+    return this.afStorage.ref(`files/${newName}`).putString(information);
+  }
+
+  storeInfoToDatabase(metainfo) {
+    let toSave = {
+      created: metainfo.timeCreated,
+      url: metainfo.downloadURLs[0],
+      fullPath: metainfo.fullPath,
+      contentType: metainfo.contentType
+    }
+    return this.dbFire.list('files').push(toSave);
+  }
+
+
+  deleteFile(file) {
+    let key = file.key;
+    let storagePath = file.fullPath;
+
+    let ref = this.dbFire.list('files');
+
+    ref.remove(key);
+    return this.afStorage.ref(storagePath).delete();
   }
 }
