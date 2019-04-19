1f6a996e76b8333450f491fcec2d5a70b104c1ae
johannag285 <johannag285@gmail.com>
2019-04-19 18:48:21 -0500

    changes reverted

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { Storage } from '@ionic/storage';
-import{Todo, TodoService} from './../services/todo.service';
+import{ TodoService} from './../services/todo.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit{
-  todos: Todo[];
+ 
  constructor(private storage: Storage,public router: Router,private todoService:TodoService){
  }
 
  ngOnInit(){
-  
  }
 
  
