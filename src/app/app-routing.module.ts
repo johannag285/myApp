2f0d250c3f75ab06d230336a20a381fd33a62f7c
Daniela <danacepeda253@gmail.com>
2019-05-18 23:33:32 -0500

    Update app-routing.module.ts
    The page guard app-router was modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import{NologinGuard} from "./guards/nologin.guard"
 import{NologinEmpresaGuard} from "./guards/nologinempresa.guard"
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule',canActivate : [NologinGuard] },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule'},
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule', canActivate : [NologinGuard] },
   { path: 'empleo', loadChildren: './empleo/empleo.module#EmpleoPageModule' },
   { path: 'crear', loadChildren: './crear/crear.module#CrearPageModule', canActivate : [NologinEmpresaGuard]},
   { path: 'perfil-estudiante', loadChildren: './perfil-estudiante/perfil-estudiante.module#PerfilEstudiantePageModule', canActivate : [AuthGuard]},
-  { path: 'perfil-estudiante/:id', loadChildren: './perfil-estudiante/perfil-estudiante.module#PerfilEstudiantePageModule' },
+  { path: 'perfil-estudiante/:id', loadChildren: './perfil-estudiante/perfil-estudiante.module#PerfilEstudiantePageModule',canActivate : [AuthGuard] },
   { path: 'perfil-empresa', loadChildren: './perfil-empresa/perfil-empresa.module#PerfilEmpresaPageModule',canActivate : [AuthGuard] },
-  { path: 'perfil-empresa/:id', loadChildren: './perfil-empresa/perfil-empresa.module#PerfilEmpresaPageModule' },
+  { path: 'perfil-empresa/:id', loadChildren: './perfil-empresa/perfil-empresa.module#PerfilEmpresaPageModule',canActivate : [AuthGuard] },
   { path: 'login-empresa', loadChildren: './login-empresa/login-empresa.module#LoginEmpresaPageModule', canActivate : [NologinEmpresaGuard]},
   { path: 'crear-estudiante', loadChildren: './crear-estudiante/crear-estudiante.module#CrearEstudiantePageModule',canActivate : [NologinGuard] },
   { path: 'datos-perfil-estudiante', loadChildren: './datos-perfil-estudiante/datos-perfil-estudiante.module#DatosPerfilEstudiantePageModule' },
