cd19fb404a195b380c7bc2cbd77ace831782205b
johannag285 <johannag285@gmail.com>
2019-05-23 15:56:12 -0500

    add the local storage varibel for get uid

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import{AngularFireAuth} from "@angular/fire/auth"
 import { promise } from 'protractor';
 import{Router} from "@angular/router"
+import { TodoService, Estudiante } from './../services/todo.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private AFauth:  AngularFireAuth, private router: Router) { }
-
+  constructor(private AFauth:  AngularFireAuth, private router: Router,private todoService: TodoService) { }
+  user_id_empleo:any ;
+  user_id_estudiante:any ;
   login(email:string,password:string){
     return new Promise((resolve, rejected) =>{
       this.AFauth.auth.signInWithEmailAndPassword(email,password).then(data=>{
@@ -17,6 +19,8 @@ export class AuthService {
         //en donde resiva este uid, luego cuando me loguee hago un where en donde me traiga solo
         //los datos que correpsondan a este id
         resolve(data.user.uid);
+        this.user_id_empleo = this.todoService.getUserInfoEmpleo(data.user.uid);
+        this.user_id_estudiante= this.todoService.getUserInfoEstudiante(data.user.uid);
         
       }).catch(err =>rejected(err))
     });  
