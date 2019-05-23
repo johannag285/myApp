eccab4a296fee246ff502b17db757b7a343ea289
johannag285 <johannag285@gmail.com>
2019-05-23 15:58:53 -0500

    add the changes for employs

--- src/app/empleo/empleo.page.ts
+++ src/app/empleo/empleo.page.ts
@@ -9,15 +9,18 @@ export class EmpleoPage implements OnInit {
   showCreate =false;
   empleos: Empleo[];
   esEstudiante = '';
+  esEmpresa = '';
   constructor(private todoService: TodoService) { }
 
   ngOnInit() {
     //leo la variable del storage
     this.esEstudiante = localStorage.getItem('estudiante');
-    let variable;
-     if(variable == true){
+    this.esEmpresa = localStorage.getItem('empresa');
+
+     if(this.esEstudiante == '1'){
        this.showCreate = true;
      }
+     
 
     this.todoService.getEmpleos().subscribe(res => {
       this.empleos = res;
